e83097daed4d111e1da6a4bb2f93113d658bea52
alex.zhang <gzane24@hotmail.com>
2021-06-19 15:47:35 +0800

    feat: add new koa-access-token

---
+++ packages/koa-access-token/src/AccessData.ts
@@ -0,0 +1,239 @@
+import crypto from 'crypto';
+import ms from "ms";
+import { Commands } from "@augejs/redis";
+
+const DEFAULT_ACCESS_TOKE_KEY_PREFIX = 'access';
+
+export interface FindSessionsByUserIdOpts {
+  skipCount?: number,
+  incudesCurrent?: boolean,
+}
+
+export interface AccessData {
+  readonly token: string
+  readonly userId: string
+  readonly nonce: string
+  readonly fingerprint: string
+  readonly ip: string
+  readonly createAt: number
+  readonly updateAt: number
+  readonly hasSaved: boolean;
+
+  maxAge: number
+  isDeadNextTime: boolean
+  flashMessage: string | null
+
+  setMaxAge(maxAge: string | number): void
+  
+  set(key: string, val: unknown): void
+  get<T=unknown>(key: string): T
+  
+  toJSON(): Record<string, unknown>
+  active(): Promise<void>
+  commit(): void
+  save(force?: boolean): Promise<void> 
+  delete(): Promise<void>
+}
+
+function getRedisKeyFromAccessToken(accessToken: string): string {
+  return Buffer.from(accessToken, 'base64').toString('utf8');
+}
+
+function getAccessionTokenFromRedisKey(redisKey: string): string {
+  return Buffer.from(redisKey, 'base64').toString('base64');
+}
+
+export class AccessDataImpl implements AccessData {
+  static create(redis: Commands, userId:string, ip: string, maxAge: string | number): AccessData {
+    const maxAgeNum = typeof maxAge === 'string' ? ms(maxAge) : maxAge;
+    const timestamp = Date.now();
+    const nonce: string = crypto.randomBytes(32).toString('hex');
+    const hash: string = crypto.createHash('md5').update(`${userId}-${ip}-${nonce}-${timestamp}`).digest('hex');
+    const redisKey = `${DEFAULT_ACCESS_TOKE_KEY_PREFIX}:${userId}:${hash}`;
+    const accessToken = getAccessionTokenFromRedisKey(redisKey);
+
+    const accessData = new AccessDataImpl(redis, {
+      token: accessToken,
+      userId,
+      maxAge: maxAgeNum,
+      nonce,
+      ip,
+      createAt: timestamp,
+      updateAt: timestamp,
+    });
+
+    return accessData;
+  }
+
+  static async find(redis: Commands, accessToken: string): Promise<AccessData | null> {
+    const redisKey = getRedisKeyFromAccessToken(accessToken);
+
+    const jsonResultStr = await redis.get(redisKey);
+    if (!jsonResultStr) {
+      return null;
+    }
+
+    try {
+      return new AccessDataImpl(redis, JSON.parse(jsonResultStr));
+    } catch (err) {
+      return null;
+    }
+  }
+
+  static async findSessionsByUserId(redis: Commands, userId: string, currentAccessToken: string | null, opts?: FindSessionsByUserIdOpts): Promise<AccessData[]> {
+    let results: AccessData[] = [];
+
+    const skipCount = opts?.skipCount ?? 0;
+    const incudesCurrent = opts?.incudesCurrent ?? false;
+  
+    const redisKeys: string[] = await redis.keys(`${DEFAULT_ACCESS_TOKE_KEY_PREFIX}:${userId}:*`);
+    if (redisKeys.length === 0) return results;
+
+    for (const redisKey of redisKeys) {
+      // fix the first prefix
+      const accessToken: string = getAccessionTokenFromRedisKey(redisKey);
+      const accessData = await AccessDataImpl.find(redis, accessToken);
+      if (!accessData) continue;
+      if (!incudesCurrent && currentAccessToken && currentAccessToken === accessData.token) continue;
+
+      results.push(accessData);
+    }
+
+    // last login is the top one
+    results.sort((a:AccessData, b: AccessData ) => {
+      return b.createAt - a.createAt;
+    });
+
+    if (skipCount > 0) {
+      results = results.slice(skipCount);
+    }
+  
+    return results;
+  }
+
+  static async delete(redis: Commands, sessionId: string): Promise<void> {
+    const redisKey = getRedisKeyFromAccessToken(sessionId);
+    await redis.del(redisKey);
+  }
+
+  private redis: Commands;
+  private redisKey: string;
+
+  private dataDirty = false;
+  private _hasSaved = false;
+  private data: Record<string, unknown> = {};
+
+  private constructor(redis: Commands, data: Record<string, unknown>) {
+    Object.assign(this.data, data);
+    this.redis = redis;
+    this.redisKey = getRedisKeyFromAccessToken(this.token);
+  }
+
+  get token(): string {
+    return this.get<string>("token") ?? "";
+  }
+
+  get userId(): string {
+    return this.get<string>("userId") ?? "";
+  }
+
+  get nonce(): string {
+    return this.get<string>("nonce") ?? "";
+  }
+
+  get fingerprint(): string {
+    return this.get<string>("fingerprint") ?? "";
+  }
+
+  get ip(): string {
+    return this.get<string>("ip") ?? "";
+  }
+
+  get createAt(): number {
+    return this.get<number>("createAt") ?? 0;
+  }
+
+  get updateAt(): number {
+    return this.get<number>("updateAt") ?? 0;
+  }
+
+  get maxAge(): number {
+    return this.get<number>("maxAge") ?? 0;
+  }
+
+  set maxAge(val: number) {
+    this.set("maxAge", val);
+  }
+
+  setMaxAge(maxAge: string | number): void {
+    this.set("maxAge", typeof maxAge === 'string' ? ms(maxAge) : maxAge);
+  }
+
+  get isDeadNextTime(): boolean {
+    return !!this.get("_isDeadNextTime");
+  }
+
+  set isDeadNextTime(val: boolean) {
+    this.set("_isDeadNextTime", val);
+  }
+
+  get hasSaved(): boolean {
+    return this._hasSaved;
+  }
+
+  get flashMessage() : string | null {
+    return this.get<string | null>("_flashMessage") ?? null;
+  }
+
+  set flashMessage(val: string | null) {
+    this.set("_flashMessage", val === null ? undefined : val);
+  }
+
+  set(key: string, val: unknown): void {
+    if (this.data[key] === val) return;
+
+    if (val === undefined) {
+      delete this.data[key];
+    } else {
+      this.data[key] = val;
+    }
+    
+    this.dataDirty = true;
+  }
+
+  get<T=unknown>(key: string): T {
+    return this.data[key] as T;
+  }
+
+  toJSON(): Record<string, unknown> {
+    const results: Record<string, unknown> = {};
+    for (const [key, value] of Object.entries(this.data)) {
+      if (key.startsWith('_')) continue;
+      results[key] = value;
+    }
+    return results;
+  }
+
+  async active(): Promise<void> {
+    await this.redis.pexpire(this.redisKey, this.maxAge);
+  }
+
+  commit(): void {
+    if (this.dataDirty) {
+      this.data.updateAt = Date.now();
+    }
+  }
+
+  async save(force?: boolean): Promise<void> {
+    if (!force && !this.dataDirty) {
+      return;
+    }
+    await this.redis.set(this.redisKey, JSON.stringify(this.toJSON()), 'PX', this.maxAge);
+    this.dataDirty = false;
+    this._hasSaved = true;
+  }
+
+  async delete(): Promise<void> {
+    await this.redis.del(this.redisKey);
+  }
+}
---
+++ packages/koa-access-token/src/main.ts
@@ -0,0 +1,210 @@
+import crypto from 'crypto';
+
+import { Config, LifecycleOnInitHook, Logger, Metadata, ScanContext, ScanNode } from '@augejs/core';
+import { KOA_WEB_SERVER_IDENTIFIER, MiddlewareFactory, HttpStatus, KoaApplication, KoaContext } from '@augejs/koa';
+import { I18N_IDENTIFIER, II18n } from '@augejs/i18n';
+import { FindSessionsByUserIdOpts, AccessData, AccessDataImpl } from './AccessData';
+import { REDIS_IDENTIFIER, Commands } from '@augejs/redis';
+
+const ACCESS_TOKEN_IDENTIFIER = 'accessToken';
+const DEFAULT_ACCESS_TOKE_MAX_AGE= '20m';
+
+const logger = Logger.getLogger(ACCESS_TOKEN_IDENTIFIER);
+
+declare module '@augejs/koa' {
+  interface KoaContext {
+    createAccessData(userId:string, maxAge: string | number): AccessData;
+    findAccessData(sessionId: string): Promise<AccessData | null>
+    findAccessDataListByUserId(userId: string, opts?: FindSessionsByUserIdOpts): Promise<AccessData[]>
+    deleteAccessData(sessionId: string):Promise<void>
+
+    accessData: AccessData | null
+  }
+}
+
+interface FingerPrintOptions {
+  userAgent?: boolean
+  ip?: boolean
+  deviceUUId?:boolean
+}
+
+type CalculateFingerPrintFunction =  (ctx: KoaContext, fingerPrintOptions?: FingerPrintOptions) => Promise<string> | string;
+
+interface AccessDataConfigOptions {
+  maxAge?: string | number
+  autoKeepActive?: boolean
+  fingerPrintOptions?: FingerPrintOptions
+  customCalculateFingerPrint?: CalculateFingerPrintFunction
+}
+
+export function AccessTokenManager(opts?: AccessDataConfigOptions): ClassDecorator {
+  return function(target: CallableFunction) {
+    Metadata.decorate([
+      Config({
+        [ACCESS_TOKEN_IDENTIFIER]: {
+          // this will auto keep active for every request.
+          maxAge: DEFAULT_ACCESS_TOKE_MAX_AGE,
+          autoKeepActive: true,
+          ...opts,
+        }
+      }),
+
+      LifecycleOnInitHook(async (scanNode: ScanNode, next: CallableFunction) => {
+        const context: ScanContext = scanNode.context;
+        const koa = context.container.get<KoaApplication>(KOA_WEB_SERVER_IDENTIFIER);
+        const redis = context.container.get<Commands>(REDIS_IDENTIFIER);
+
+        const config: AccessDataConfigOptions = {
+          ...scanNode.context.rootScanNode!.getConfig(ACCESS_TOKEN_IDENTIFIER),
+          ...scanNode.getConfig(ACCESS_TOKEN_IDENTIFIER),
+        };
+
+        koa.context.calculateFingerprint = async function (): Promise<string> {
+          const ctx = this as KoaContext;
+          if (typeof config?.customCalculateFingerPrint === 'function') {
+            return await config.customCalculateFingerPrint(ctx, config.fingerPrintOptions) ?? '';
+          }
+          const deviceId = config.fingerPrintOptions?.deviceUUId ? ctx.get('device-uuid') : '';
+          const ip = config?.fingerPrintOptions?.ip ? ctx.ip : '';
+          const userAgent: string = config?.fingerPrintOptions?.userAgent ? ctx.get('user-agent') : '';
+          return crypto.createHash('md5').update(`${deviceId}${ip}${userAgent}`).digest('hex');
+        }
+
+        koa.context.createAccessData = function(userId:string, maxAge: string | number): AccessData {
+          const ctx = this as KoaContext;
+          const ip = ctx.ip;
+          const currentMaxAge: string | number = (maxAge ?? config.maxAge ?? DEFAULT_ACCESS_TOKE_MAX_AGE) as string | number;
+          return AccessDataImpl.create(redis, userId, ip, currentMaxAge);
+        }
+
+        koa.context.findAccessData = async function(sessionId: string): Promise<AccessData | null> {
+          return await AccessDataImpl.find(redis, sessionId);
+        }
+
+        koa.context.findAccessDataListByUserId = async function (userId: string, opts?: FindSessionsByUserIdOpts): Promise<AccessData[]> {
+          const ctx = this as KoaContext;
+          const currentSessionId = ctx.accessData?.token ?? null;
+          return await AccessDataImpl.findSessionsByUserId(redis, userId, currentSessionId, opts);
+        }
+
+        koa.context.deleteAccessData = async function(sessionId: string):Promise<void> {
+          await AccessDataImpl.delete(redis, sessionId);
+        }
+
+        await next();
+      })
+    ], target);
+  }
+}
+
+
+type AccessTokenMiddlewareOptions = {
+  optional?: boolean,
+  autoActive?: boolean,
+  autoSave?: boolean,
+  checkFingerprint?: boolean,
+}
+
+// https://github.com/koajs/bodyparser
+export function AccessTokenMiddleware(opts?: AccessTokenMiddlewareOptions): ClassDecorator & MethodDecorator {
+  return MiddlewareFactory(async (scanNode: ScanNode) => {
+    const i18n:II18n = scanNode.context.container.get(I18N_IDENTIFIER);
+
+    const config: AccessTokenMiddlewareOptions = {
+      autoActive: true,
+      autoSave: true,
+      ...scanNode.context.rootScanNode!.getConfig(ACCESS_TOKEN_IDENTIFIER),
+      ...scanNode.getConfig(ACCESS_TOKEN_IDENTIFIER),
+      ...opts,
+    };
+
+    const autoSave = !!config.autoSave;
+    const autoActive = !!config.autoActive;
+    const checkFingerprint = !!config.checkFingerprint;
+    const optional = !!config?.optional;
+
+    return async (ctx: KoaContext, next: CallableFunction) => {
+      const accessToken:string = ctx.get('Authorization') ?? ctx.request.body?.get('access_token') ?? ctx.request.get('access_token');
+      if (!accessToken) {
+        if (optional) {
+          await next();
+          return;
+        } else {
+          // https://github.com/ValueFE/egg-access-token/blob/1f3718bc6a71c548236facc5694e96263a20daa6/app/middleware/accessToken.js#L21
+          // todo throw error here
+          logger.warn(`ip: ${ctx.ip} accessToken is required!`);
+
+          ctx.throw(HttpStatus.StatusCodes.UNAUTHORIZED, 
+            i18n.formatMessage({
+              id: 'Error_Missing_Access_Token',
+              defaultMessage: 'AccessToken is Required'
+            })
+          )
+        }
+      }
+
+      const accessData = await ctx.findAccessData(accessToken);
+
+      if (!accessData) {
+        if (optional) {
+          await next();
+          return;
+        } else {
+          // todo throw error here
+          // https://github.com/ValueFE/egg-access-token/blob/1f3718bc6a71c548236facc5694e96263a20daa6/app/middleware/accessToken.js#L35
+          logger.warn(`ip: ${ctx.ip} accessToken is invalid!`);
+          ctx.throw(HttpStatus.StatusCodes.UNAUTHORIZED, 
+            i18n.formatMessage({
+              id: 'Error_Invalid_Access_Token',
+              defaultMessage: 'AccessToken Is Invalid'
+            })
+          )
+        }
+      }
+
+      if (accessData.isDeadNextTime) {
+        const flashMessage = accessData.flashMessage ?? '';
+        logger.warn(`userId: ${accessData.userId} accessToken is ready to invalid. message: ${flashMessage}`);
+        await accessData.delete();
+        ctx.throw(HttpStatus.StatusCodes.UNAUTHORIZED, flashMessage);
+      }
+
+      if (checkFingerprint) {
+        const calculatedFingerprint = ctx.calculateFingerprint();
+        if (accessData.fingerprint !== calculatedFingerprint) {
+          await accessData.delete();
+          logger.warn(`userId: ${accessData.userId} fingerprint is invalid expect: ${calculatedFingerprint} receive: ${accessData.fingerprint}`);
+        
+          ctx.throw(HttpStatus.StatusCodes.UNAUTHORIZED, 
+            i18n.formatMessage({
+              id: 'Error_Invalid_Client_Fingerprint',
+              defaultMessage: 'Client fingerprint is changed!'
+            })
+          )
+        }
+      }
+
+      ctx.accessData = accessData;
+
+      await next();
+
+      if (!ctx.accessData) {
+        await accessData.delete();
+        return;
+      }
+
+      if (accessData.flashMessage) {
+        accessData.flashMessage = null;
+      }
+
+      if (autoSave) {
+        await accessData.save();
+      }
+
+      if (autoActive && !accessData.hasSaved) {
+        await accessData.active();
+      }
+    }
+  })
+}
+
