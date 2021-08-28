e7952bd528268a27a905903dee1c872a539839b1
alex.zhang <gzane24@hotmail.com>
2021-08-29 00:17:52 +0800

    feat: add cors to security

--- packages/koa-security/src/main.ts
+++ packages/koa-security/src/main.ts
@@ -1,6 +1,7 @@
 
 import KoaHelmet from 'koa-helmet';
 import helmet from 'helmet';
+import cors from 'koa2-cors';
 
 import { 
   ScanNode
@@ -11,17 +12,27 @@ import { MiddlewareFactory } from '@augejs/koa';
 export const ConfigName = 'security';
 
 type HelmetOptions = Required<Parameters<typeof helmet>>[0];
+type CorsOptions = {
+  cors?: cors.Options
+}
 
-export function KoaSecurityMiddleware(opts?: HelmetOptions | CallableFunction): ClassDecorator & MethodDecorator {
+type SecurityOptions = HelmetOptions & CorsOptions
+
+export function KoaSecurityMiddleware(opts?: SecurityOptions | CallableFunction): ClassDecorator & MethodDecorator {
   return MiddlewareFactory(async (scanNode: ScanNode) => {
     if (typeof opts === 'function') {
       opts = await opts(scanNode);
     }
 
-    return KoaHelmet({
+    const config: SecurityOptions = {
       ...scanNode.context.rootScanNode!.getConfig(ConfigName),
       ...scanNode.getConfig(ConfigName),
       ...opts
-    });
+    }
+
+    return [
+      cors(config.cors),
+      KoaHelmet(config)
+    ];
   });
 }
